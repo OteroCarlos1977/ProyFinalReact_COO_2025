6799eb0bad1a4073ddd6744017e4053cc0209647
Carlos Octavio Otero <166446069+OteroCarlos1977@users.noreply.github.com>
2025-05-27 19:19:49 -0300

    Cambio git page
    Cambio configuración para gitpage

--- react2025/vite.config.js
+++ react2025/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://oterocarlos99.github.io/ProyFinalReact_COO_2025',
   plugins: [react()],
 })
